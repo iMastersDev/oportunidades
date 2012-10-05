071f90775f3c4f744f40e2df13fae65a42abf1c8
Ramon Almeida <contato@ramonalmeida.com>
2012-10-05 18:49:40 -0300

    Alteracao no html da view index

--- public/views/index.php
+++ public/views/index.php
@@ -1,44 +1,104 @@
-<!doctype html>
-<html>
+<!DOCTYPE HTML>
+<html lang="pt-BR">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>oPHPortunidades</title>
     <link rel="stylesheet" type="text/css" href="../static/css/default.css">
 </head>
 <body>
-    <div class="wrapper">
+    <header>
         <div class="container">
-            <header>
-                <div class="nav-wrapper">
-                    <h1>
-                        oPHPoportunidades
-                    </h1>
-                    <nav>
-                        <ul>
-                            <li>home</li>
-                            <li>oportunidades</li>
-                            <li>sobre</li>
-                        </ul>
-                    </nav>
+            <div class="logo">
+                <h1>
+                    o<span>php</span>ortunidades
+                </h1>
+            </div>
+            <nav>
+                <ul>
+                    <!-- Menu Items -->
+                    <li class="current-menu-item"><a href="#">Home</a></li>
+                    <li><a href="#">Oportunidades</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <!-- /Menu Items -->
+                </ul>
+            </nav>
+            <div class="action-wrapper">
+                <div class="search">
+                    <form action="">
+                        <input type="search" placeholder="Procurar Oportunidades" />
+                        <input type="submit" value="Procurar" />
+                    </form>
                 </div>
-                <div class="actions-wrapper">
-                    <div class="search"></div>
-                    <div class="post-button"></div>
-                </div>
-            </header>
-            <article>
-                <h3>Últimas Vagas</h3>
-            </article>
-            <footer>
-                <span>Ver todas oportunidades</span>
-                <div class="search"></div>
-                <div class="infos">
-                    <span>Organização:</span>
-                    <span>Hospedagem:</span>
-                    <span>Fork us on:</span>
-                </div>
-            </footer>
+            </div>
+            <div class="post-button">
+                <a href="#" id="newJob">Postar nova Vaga!</a>
+            </div>
         </div>
-    </div>
+    </header>
+    <section id="last-jobs">
+        <header>
+            <h2>05 Últimas Vagas</h2>
+        </header>
+        <ul>
+            <!-- List of Jobs -->
+            <li>
+                <a href="#">
+                    <span>05/10</span>Lorem ipsum dolor sit amet.
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>04/10</span>Pellentesque venenatis dapibus lacus quis feugiat
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>03/10</span>Lorem ipsum dolor sit amet.
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>02/10</span>Pellentesque venenatis dapibus lacus quis feugiat
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>01/10</span>Lorem ipsum dolor sit amet.
+                </a>
+            </li>
+            <!-- /List of Jobs -->
+        </ul>
+        <aside>
+            <a href="#">
+                <i class="icon icon-suitcase"></i>Ver todas oportunidades</a>
+            <div class="search">
+                <form action="">
+                    <input type="search" placeholder="Procurar Oportunidades" />
+                    <input type="submit" value="Procurar" />
+                </form>
+            </div>
+        </aside>
+    </section>
+    <footer>
+        <div class="infos">
+            <ul>
+                <li>
+                    <a href="#" class="imasters">
+                        Organização: <span class="hide">iMasters</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="hosting">
+                        Hospedagem: <span class="hide">A definir</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="github">
+                        Fork us on: <span class="hide">GitHub</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </body>
 </html>
