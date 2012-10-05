baecd186dda4337060ad330e2203d622de9c0716
Daniel Ribeiro <drgomesp@gmail.com>
2012-10-05 06:35:16 -0300

    Inclui Vagrantfile para máquina com Puppet para provisionamento

---
+++ puppet/manifests/ophportunidades.pp
@@ -0,0 +1,67 @@
+class ophportunidades {
+
+    exec { "yum update":
+        command => "yum -y update",
+        path => "/usr/bin",
+    }
+
+    package { "git":
+        ensure => present,
+    }
+}
+
+class apache {
+    
+    package { "httpd":
+        ensure => present,
+    }
+
+    package { "httpd-devel":
+        ensure => present,
+    }
+
+    service { "httpd":
+        ensure => running,
+        require => Package["httpd"],
+    }
+}
+
+class mysql {
+    
+    package { "mysql":
+        ensure => present,
+    }
+
+    package { "mysql-server":
+        ensure => present,
+    }
+
+    package { "mysql-devel":
+        ensure => present,
+    }
+
+    service { "mysqld":
+        ensure => running,
+        require => Package["mysql-server"],
+    }
+}
+
+class php {
+    
+    package { "php":
+        ensure => present,
+    }
+
+    package { "php-mysql":
+        ensure => present,
+    }
+
+    package { "php-common":
+        ensure => present,
+    }
+}
+
+include ophportunidades
+include apache
+include mysql
+include php
