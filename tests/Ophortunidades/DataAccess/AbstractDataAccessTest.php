28a80319ff6910421b4dc92504e391d537a47167
João Batista Neto <neto.joaobatista@gmail.com>
2012-10-05 13:59:09 -0300

    Adicionando camada de negócios da aplicação

--- tests/Ophortunidades/DataAccess/AbstractDataAccessTest.php
+++ tests/Ophortunidades/DataAccess/AbstractDataAccessTest.php
@@ -5,15 +5,16 @@ use \PDO;
 
 abstract class AbstractDataAccessTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * @var \PDO
-	 */
-	protected $pdo;
-	
-	protected function setUp()
-	{
-		$this->pdo = new PDO('sqlite::memory:');
-		$this->pdo->exec('
+    /**
+     *
+     * @var \PDO
+     */
+    protected $pdo;
+
+    protected function setUp()
+    {
+        $this->pdo = new PDO('sqlite::memory:');
+        $this->pdo->exec('
             CREATE TABLE IF NOT EXISTS position (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
@@ -21,10 +22,10 @@ abstract class AbstractDataAccessTest extends \PHPUnit_Framework_TestCase
                 place TEXT NOT NULL
             );
         ');
-	}
-	
-	protected function tearDown()
-	{
-		$this->pdo->exec('DROP TABLE position');
-	}
+    }
+
+    protected function tearDown()
+    {
+        $this->pdo->exec('DROP TABLE position');
+    }
 }
