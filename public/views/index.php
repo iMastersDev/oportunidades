6d0200a3236a0cf746cd6ca6e1c131bdd24f8405
Wesley Victhor Mendes <w.v.mendes.s@gmail.com>
2012-10-05 14:36:35 -0300

    Adiciona estrutura base de template para GUI.

---
+++ public/views/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>oPHPortunidades</title>
+    <link rel="stylesheet" type="text/css" href="../static/css/default.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <header>
+                <div class="nav-wrapper">
+                    <h1>
+                        oPHPoportunidades
+                    </h1>
+                    <nav>
+                        <ul>
+                            <li>home</li>
+                            <li>oportunidades</li>
+                            <li>sobre</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="actions-wrapper">
+                    <div class="search"></div>
+                    <div class="post-button"></div>
+                </div>
+            </header>
+            <article>
+                <h3>Últimas Vagas</h3>
+            </article>
+            <footer>
+                <span>Ver todas oportunidades</span>
+                <div class="search"></div>
+                <div class="infos">
+                    <span>Organização:</span>
+                    <span>Hospedagem:</span>
+                    <span>Fork us on:</span>
+                </div>
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
